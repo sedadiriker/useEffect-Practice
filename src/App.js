d6cac607f792cdf79094b04a83bd6343bd5723cc
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-04-18 15:29:37 +0300

    add listeningEvent component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ListeningEvent from "./pages/ListeningEvent";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/listeningevent" element={<ListeningEvent/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
